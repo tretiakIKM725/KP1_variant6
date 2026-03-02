56a117507bdb3d66922a110b0d27fb1d06b77b38
tretiakIKM725vdsg <yaroslavtretiakuni@gmail.com>
2026-03-02 10:12:31 +0100

    Add files via upload

---
+++ zavdanya2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  int n;
+  long long factorial = 1;
+
+  cout << "Bвeдiть число : ";
+  cin >> n;
+
+  if (n < 0) {
+    cout << 'Помилка !' << endl;
+  } else {
+    while(n > 0) {        // або 1 щоб менше крокiв було
+      factorial *= n;
+      n--;
+    }
+    cout << "Результат : " << factorial << endl;
+  }
+  
+  return 0;
+}
